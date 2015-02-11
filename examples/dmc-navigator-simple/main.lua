a7910b216902a9f7d204cf44a81825170ec1de72
dmc <my-github@davidmccuskey.com>
2015-02-11 15:58:59 -0700

    update navigator example, page removal

--- examples/dmc-navigator-simple/main.lua
+++ examples/dmc-navigator-simple/main.lua
@@ -42,15 +42,17 @@ local H_CENTER, V_CENTER = W*0.5, H*0.5
 local navigator, nav_bar -- set later
 local o
 
-local createGalleriesView, galleriesView_handler
-local createGalleryView, galleryView_handler
-local createImageView, imageView_handler
+local createGalleriesView, removeGalleriesView, galleriesView_handler
+local createGalleryView, removeGalleryView, galleryView_handler
+local createImageView, removeImageView, imageView_handler
 
 
 
 --====================================================================--
 --== Support Functions
 
+
+--== Galleries
 
 createGalleriesView = function( galleries )
 	-- print( "createGalleriesView", galleries )
@@ -62,15 +64,22 @@ createGalleriesView = function( galleries )
 	o:addEventListener( o.EVENT, galleriesView_handler )
 	return o
 end
+removeGalleriesView = function( o )
+	-- print( "removeGalleriesView", o )
+	o:removeEventListener( o.EVENT, galleriesView_handler )
+	o:removeSelf()
+end
 
 galleriesView_handler = function( event )
-	print( "Main:galleriesView_handler", event.type )
-	Utils.print( event )
+	-- print( "Main:galleriesView_handler", event.type )
+	-- Utils.print( event )
 	local gallery_data = event.data
 	local o = createGalleryView( gallery_data )
 	navigator:pushView( o )
 end
 
+
+--== Gallery
 
 createGalleryView = function( gallery )
 	local o = GalleryView:new{
@@ -81,15 +90,21 @@ createGalleryView = function( gallery )
 	o:addEventListener( o.EVENT, galleryView_handler )
 	return o
 end
-
+removeGalleryView = function( o )
+	-- print( "removeGalleryView", o )
+	o:removeEventListener( o.EVENT, galleryView_handler )
+	o:removeSelf()
+end
 galleryView_handler = function( event )
-	print( "Main:galleryView_handler", event.type )
+	-- print( "Main:galleryView_handler", event.type )
+	-- Utils.print( event )
 	local image_data = event.data
-	Utils.print( image_data )
 	local o = createImageView( image_data )
 	navigator:pushView( o )
 end
 
+
+--== Image
 
 createImageView = function( image )
 	local o = ImageView:new{
@@ -100,10 +115,31 @@ createImageView = function( image )
 	o:addEventListener( o.EVENT, imageView_handler )
 	return o
 end
-
+removeImageView = function( o )
+	-- print( "removeImageView", o )
+	o:removeEventListener( o.EVENT, imageView_handler )
+	o:removeSelf()
+end
 imageView_handler = function( event )
 	-- print( "Main:imageView_handler", event.type )
 	-- none
+end
+
+
+local function navigatorEvent_handler( event )
+	-- print( "Main:navigatorEvent_handler", event.type )
+	local nav = event.target
+	if event.type == nav.REMOVED_VIEW then
+		assert( event.view )
+		local view = event.view
+		if view:isa( GalleriesView ) then
+			removeGalleriesView( view )
+		elseif view:isa( GalleryView ) then
+			removeGalleryView( view )
+		elseif view:isa( ImageView ) then
+			removeImageView( view )
+		end
+	end
 end
 
 
@@ -112,33 +148,28 @@ end
 --====================================================================--
 
 
---== Create Navigator and Nav Bar
+-- create nav bar
 
 nav_bar = Widgets.newNavBar{
 	width=W
 }
 nav_bar.x, nav_bar.y = H_CENTER, 0
 
+-- create navigator
 
 navigator = Navigator:new{
 	width=W,
 	height=H-nav_bar.HEIGHT,
 	default_reference=display.TopCenterReferencePoint
 }
--- o:setAnchor( o.TopCenterReferencePoint )
 navigator.x, navigator.y = H_CENTER, 0+nav_bar.HEIGHT
+navigator:addEventListener( navigator.EVENT, navigatorEvent_handler )
 
-navigator.nav_bar = nav_bar
+navigator.nav_bar = nav_bar -- set nav bar delegate
 
 
---== Create Root Gallery Page
+-- create root Galleries View, and push
 
 o = createGalleriesView( galleries_data )
 navigator:pushView( o )
-
-
--- timer.performWithDelay( 1000, function()
--- 	o = createGalleryView( galleries_data[1] )
--- 	navigator:pushView( o )
--- end)
 
